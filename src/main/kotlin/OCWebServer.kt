550f1022bd347a0772331acf654a528b577a976e
Genmare <gregfi@aliceadsl.fr>
2019-03-20 22:25:11 +0100

    Activité : Développez une API en Kotlin et Ktor

---
+++ src/main/kotlin/OCWebServer.kt
@@ -0,0 +1,63 @@
+package ocwebserver
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.jackson.jackson
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+import ocwebserver.extensions.bold
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(
+        Netty,
+        port = 8080,
+        module = Application::mymodule
+        ).apply { start(wait = true) }
+}
+
+fun Application.mymodule() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+        }
+    }
+    routing {
+        get("/") {
+            var bienVenue : String = "Welcome to OpenClassrooms brand new server !"
+            bienVenue =bienVenue.bold()
+//            println(bienVenue)
+            call.respondText(bienVenue, ContentType.Text.Html)
+        }
+        get("/course/top") {
+            call.respond(Cours(1, "How to troll", 5, true))
+        }
+        get("course/{id}") {
+
+            val num = call.parameters["id"]
+
+            val cours : Any = when(num) {
+                "1" -> Cours(1, "How to troll", 5, true)
+                "2" -> Cours(2, "Kotlin for troll", 1, true)
+                "3" -> Cours(3, "Ktor the troll", 3)
+                else -> Message()
+            }
+
+            call.respond(cours)
+        }
+    }
+}
+
+data class Cours(val id : Int, val title : String, val level : Int, var isActive : Boolean = false)
+
+class Message(val status : Int = 404, val message : String = "Sorry! No course were found...")
+
+
---
+++ src/main/kotlin/extensions/StringExtensions.kt
@@ -0,0 +1,3 @@
+package ocwebserver.extensions
+
+fun String.bold():String = "<p><b><u><i>$this</i></u></b></p>"
